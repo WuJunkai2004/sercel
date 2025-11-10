7870cc1c17f31ecc0ca40226b35aebb267a168b3
Wu Junkai <wu.junkai@qq.com>
2025-11-10 16:16:35 +0800

    feat: 删除 hello.py 文件，移除不再需要的功能

--- hello.py
+++
@@ -1,8 +0,0 @@
-from sercel import vercel
-from sercel import decorators as dec
-
-@vercel.register
-@dec.hot_reload
-def hello(response):
-    response.send_code(200)
-    response.send_text("Hello, world!")
