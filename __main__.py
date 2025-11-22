e04d9ba9034d68b471dc3629806a57ebae1ea08b
Wu Junkai <wu.junkai@qq.com>
2025-11-22 11:56:51 +0800

    feat: 添加命令行参数支持以动态设置服务器端口

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
 from . import vercel
 from . import verapi
 
+import argparse
+
+parser = argparse.ArgumentParser(description="Start the Sercel server.")
+parser.add_argument("-p", "--port", type=int, default=15444, help="Port number to run the server on (default: 15444)")
+args = parser.parse_args()
+
 if __name__ == "__main__":
     vercel.start(
         HandlerClass=verapi.handler,
-        port=15444
+        port=args.port
     )
